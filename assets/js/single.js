87a02d92b7a0098c335d85c9823b1da951ee02c3
Dario Gomez <dariogomezaguirre@gmail.com>
2021-06-09 20:08:53 -0500

    6.4.5 handle errors

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,5 +1,22 @@
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
+
+var getRepoName = function () {
+  // grab repo name from url query string
+  var queryString = document.location.search;
+  var repoName = queryString.split("=")[1];
+
+  if (repoName) {
+    // display repo name on the page
+    repoNameEl.textContent = repoName;
+    getRepoIssues(repoName);
+  } else {
+    // if no repo was given, redirect to the homepage
+    document.location.replace("./index.html");
+  }
+  console.log(repoName);
+};
 
 var getRepoIssues = function (repo) {
   var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -13,9 +30,10 @@ var getRepoIssues = function (repo) {
 
       // check if api has paginated issues
       if (response.headers.get("Link")) {
-        console.log("repo has more than 30 issues");
+        displayWarning(repo);
       } else {
-        alert("There was a problem with your request!");
+        // if not successfull, redirect to homepage
+        document.location.replace("./index.html");
       }
     }
   });
@@ -70,4 +88,4 @@ var displayWarning = function (repo) {
   limitWarningEl.appendChild(linkEl);
 };
 
-getRepoIssues("facebook/react");
+getRepoName();
